af3fbfcd9613cef3f5888bd65681176ee68459fa
Michael Yong <wrong92@gmail.com>
2021-11-08 10:41:42 -0500

    wip

--- widget-src/code.tsx
+++ widget-src/code.tsx
@@ -1,7 +1,7 @@
-import { TableField } from "../shared/types";
+import { TableField, FieldType } from "../shared/types";
 
 const { widget } = figma;
-const { AutoLayout, Text, useSyncedState, useSyncedMap } = widget;
+const { AutoLayout, Text, useSyncedState, useSyncedMap, useEffect } = widget;
 
 type TRow = {
   rowId: string;
@@ -40,44 +40,84 @@ class SyncedTable {
   deleteRow(rowId: string): void {
     this.rows.delete(rowId);
   }
+
+  getRows(): TRow["rowData"][] {
+    const rowKeys = this.rows.keys();
+    rowKeys.sort();
+    return rowKeys.map((k) => this.rows.get(k));
+  }
 }
+
+const DEFAULT_SCHEMA: TableField[] = [
+  {
+    fieldName: "Step",
+    fieldType: FieldType.TEXT_SINGLE_LINE,
+  },
+  {
+    fieldName: "Description",
+    fieldType: FieldType.TEXT_MULTI_LINE,
+  },
+  {
+    fieldName: "Completed",
+    fieldType: FieldType.CHECKBOX,
+  },
+];
 
 function Table() {
   const [tableSchema, setTableSchema] = useSyncedState<TableField[]>(
     "tableSchema",
     []
   );
-  const tableRows = useSyncedMap("tableRows");
-  const tableMetadata = useSyncedMap("tableMetadata");
+  const tableMetadata = useSyncedMap<any>("tableMetadata");
+  const tableRows = useSyncedMap<TRow["rowData"]>("tableRows");
   const syncedTable = new SyncedTable(tableMetadata, tableRows);
+  useEffect(() => {
+    if (tableSchema.length === 0 && tableRows.size === 0) {
+      setTableSchema(DEFAULT_SCHEMA);
+      syncedTable.appendRow({
+        Step: "Insert Widget",
+        Description:
+          "The first step to using this widget is inserting it into FigJam! Congratulations you have completed this step.",
+        Completed: true,
+      });
+      syncedTable.appendRow({
+        Step: "Customise Fields",
+        Description:
+          "This widget supports various field types. Customize your table fields accordingly for you use case.",
+      });
+      syncedTable.appendRow({
+        Step: "Add / Edit / Remove Rows",
+        Description:
+          "Edit the rows in your widget. We'll make sure that each row follows the field type specified above.",
+      });
+    }
+  });
 
   return (
     <AutoLayout
-      direction="horizontal"
-      horizontalAlignItems="center"
-      verticalAlignItems="center"
+      direction="vertical"
       height="hug-contents"
+      width="hug-contents"
       padding={8}
-      fill="#FFFFFF"
       cornerRadius={8}
-      spacing={12}
-      onClick={async () => {
-        await new Promise((resolve) => {
-          figma.showUI(__html__);
-          figma.ui.on("message", (msg) => {
-            if (msg === "hello") {
-              figma.notify(`Hello ${figma.currentUser.name}`);
-            }
-            if (msg === "close") {
-              figma.closePlugin();
-            }
-          });
-        });
-      }}
+      spacing={8}
     >
-      <Text fontSize={32} horizontalAlignText="center">
-        Click Me
-      </Text>
+      <AutoLayout>
+        {tableSchema.map((field) => {
+          return <Text>{field.fieldName}</Text>;
+        })}
+      </AutoLayout>
+      <AutoLayout direction="vertical">
+        {syncedTable.getRows().map((row) => {
+          return (
+            <AutoLayout>
+              {tableSchema.map((field) => {
+                return <Text>{row[field.fieldName]}</Text>;
+              })}
+            </AutoLayout>
+          );
+        })}
+      </AutoLayout>
     </AutoLayout>
   );
 }
